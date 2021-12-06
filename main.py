b256faad570d6507e2cbb80cae5934d4eba6efe7
Sophia Rowe <sophia.rowe17@gmail.com>
2021-12-06 15:51:04 -0500

    First commit - this project exists! Pygame installed, running.

---
+++ main.py
@@ -0,0 +1,40 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+# Simple pygame program
+
+# Import and initialize the pygame library
+import pygame
+pygame.init()
+
+# Press the green button in the gutter to run the script.
+#if __name__ == '__main__':
+
+# Set up the drawing window
+screen = pygame.display.set_mode([500, 500])
+
+# Run until the user asks to quit
+running = True
+while running:
+
+    # Did the user click the window close button?
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # Fill the background with white
+    screen.fill((0, 0, 0))
+
+    # Draw a solid blue circle in the center
+    pygame.draw.circle(screen, (0, 0, 255), (250, 250), 75)
+
+    # Flip the display
+    pygame.display.flip()
+
+# Done! Time to quit.
+pygame.quit()
+
+
+print("Welcome to the Adventure Zone! I don't think I can call this that!")
