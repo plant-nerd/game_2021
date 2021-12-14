287cbea9816d905eadb81ecd4cbd6a4a17043fd6
Sophia Rowe <sophia.rowe17@gmail.com>
2021-12-14 12:55:11 -0500

    Color Zone!

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pygame
 pygame.init()
 dis=pygame.display.set_mode((800,600))
 pygame.display.update()
-pygame.display.set_caption('Science Adventure Zone')
+pygame.display.set_caption('Color Zone')
 
 yellow = (255, 255, 0)
 pink = (255, 0 , 255)
@@ -12,6 +12,8 @@ cyan = (0, 255, 255)
 black = (0, 0, 0)
 white = (255, 255, 255)
 green = (0, 255, 0)
+orange = (255, 140, 0)
+purple = (150, 0 , 255)
 
 game_over=False
 
@@ -22,6 +24,7 @@ x1_change = 0
 y1_change = 0
 
 clock = pygame.time.Clock()
+color = pink
 
 while not game_over:
     for event in pygame.event.get():
@@ -39,11 +42,63 @@ while not game_over:
                 x1_change = 0
             elif event.key == pygame.K_DOWN:
                 y1_change = 10
-                x1_change = 0
-
+            elif event.key == pygame.K_b:
+                color = blue
+            elif event.key == pygame.K_c:
+                color = cyan
+            elif event.key == pygame.K_p:
+                color = pink
+            elif event.key == pygame.K_r:
+                color = red
+            elif event.key == pygame.K_y:
+                color = yellow
+            elif event.key == pygame.K_w:
+                color = white
+            elif event.key == pygame.K_g:
+                color = green
+            elif event.key == pygame.K_e:
+                color = black
+            elif event.key == pygame.K_o:
+                color = orange
+            elif event.key == pygame.K_u:
+                color = purple
+            ###elif event.key == pygame.K_1:
+                #rainbow = True
+                #count = 0
+                #while rainbow == True:
+                    #if count == 0:
+                        #color = red
+                        #count += 1
+                    #elif count == 1:
+                    #    color = orange
+                     #   count += 1
+                    #elif count == 2:
+                     #   color = yellow
+                      #  count += 1
+                    #elif count == 3:
+                     #   color = green
+                      #  count += 1
+                    #elif count == 4:
+                     #   color = cyan
+                      #  count += 1
+                    #elif count == 5:
+                     #   color = blue
+                      #  count += 1
+                    #elif count == 6:
+                     #   color = purple
+                      #  count += 1
+                    #elif count == 7:
+                     #   color = pink
+                      #  count += 1
+                    #elif count >= 8:
+                     #   count = 0
+                    ### ^This crashes the game
+        if event.type == pygame.KEYUP:
+            x1_change = 0
+            y1_change = 0
     x1 += x1_change
     y1 += y1_change
-    pygame.draw.circle(dis,pink,(x1, y1), 50)
+    pygame.draw.circle(dis,color,(x1, y1), 50)
     pygame.display.update()
 
     clock.tick(30)
